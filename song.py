d23daa488fa23adfbb635b617ab4b37460bc8f44
xiSage <2932295394@qq.com>
2022-01-17 01:56:38 +0800

    Update

--- song.py
+++ song.py
@@ -13,9 +13,12 @@ with urllib.request.urlopen(req) as response:
 # 数据转存
 jdata = json.loads(data)
 song_data = {
-    "artists": {}
+    "artists": {},
+    "pvs":{}
 }
+## 标题
 song_data["title"] = jdata["defaultName"]
+## 作者
 for artist in jdata["artists"]:
     categories =  artist["categories"].split(", ")
     roles = artist["effectiveRoles"].split(", ")
@@ -26,7 +29,22 @@ for artist in jdata["artists"]:
         for role in roles:
             if role not in song_data["artists"][category]:
                 song_data["artists"][category][role] = []
-            song_data["artists"][category][role] = artist["name"]
+            song_data["artists"][category][role].append(artist["name"])
+## PV
+for pv in jdata["pvs"]:
+    if pv["pvType"] == "Original":
+        service = pv["service"]
+        if "pvID" in pv:
+            id = pv["pvID"]
+        else:
+            id = ""
+        if "publishDate" in pv:
+            date = pv["publishDate"]
+        else:
+            date = ""
+        if service not in song_data["pvs"]:
+            song_data["pvs"][service] = []
+        song_data["pvs"][service].append({"id":id,"date":date})
 print(song_data)
 # 确认数据
 # song_title = input(f"请输入歌曲标题[{song_title}]：") or song_title
